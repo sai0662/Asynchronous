12c84357f5574b5fbe009976de3d4f9a2002f84d
sai0662 <sait0662@gmail.com>
2021-09-17 23:59:36 +0530

    npm packages installed and workedon it

--- creatingownmodule.js
+++ creatingownmodule.js
@@ -1,3 +1,9 @@
-exports.myDateTime = function(){
-    return Date();
+exports.add = function(a,b){
+    return a+b;
 };
+
+
+/*
+let add = (a,b) => a+b;
+
+module.exports = add;*/
--- ownmodule.js
+++ ownmodule.js
@@ -1,9 +1,13 @@
 var http = require('http')
 
-var dt = require('./creatingownmodule');
+var add = require('./creatingownmodule');
 
 http.createServer(function(req,res){
     res.writeHead(200,{'content-type': 'text/html'});
-    res.write("the current date and time :" +dt.myDateTime());
+    res.write("sum of 2 numbers :" +add.add(5,5));
     res.end();
 }).listen(8081);
+
+/*const add = require('./creatingownmodule.js');
+
+console.log(add(4,5));*/
